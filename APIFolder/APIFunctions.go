7c0931e3366f6731002e1b27b1843ba936dbf49b
HayatEden <hayate@post.bgu.ac.il>
2022-04-29 14:54:45 +0300

    firsttt

--- APIFolder/APIFunctions.go
+++ APIFolder/APIFunctions.go
@@ -1,24 +1,16 @@
 package APIFolder
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"miniProject/EntitiesFolder"
-	"net/http"
-)
-
-func createPerson(w http.ResponseWriter, r *http.Request) {
-	var newPerson EntitiesFolder.Person
-
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
-	}
-
-	json.Unmarshal(reqBody, &newEvent)
-	events = append(events, newEvent)
-	w.WriteHeader(http.StatusCreated)
-
-	json.NewEncoder(w).Encode(newEvent)
-}
+//func createPerson(w http.ResponseWriter, r *http.Request) {
+//	var newPerson EntitiesFolder.Person
+//
+//	reqBody, err := ioutil.ReadAll(r.Body)
+//	if err != nil {
+//		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+//	}
+//
+//	json.Unmarshal(reqBody, &newEvent)
+//	events = append(events, newEvent)
+//	w.WriteHeader(http.StatusCreated)
+//
+//	json.NewEncoder(w).Encode(newEvent)
+//}
