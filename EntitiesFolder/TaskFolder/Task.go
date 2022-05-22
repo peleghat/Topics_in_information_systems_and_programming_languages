31a18ecc4119a7349b62f7d8fc7a73dbf67efac8
HayatEden <hayate@post.bgu.ac.il>
2022-05-22 18:07:24 +0300

    interfacessss

--- EntitiesFolder/TaskFolder/Task.go
+++ EntitiesFolder/TaskFolder/Task.go
@@ -1,36 +1,38 @@
 package TaskFolder
 
 type Task struct {
-	id      string
-	ownerId string
-	status  Status
+	//id      string
+	OwnerId string
+	//status  Status
 }
 
 //func NewTask(ownerId string, status Status) *Task {
 //	id := uuid.New()
 //	return &Task{id: id.String(), ownerId: ownerId, status: status}
 //}
+//
+//func (t *Task) GetId() string {
+//	return t.id
+//}
+//
+//func (t *Task) SetId(id string) {
+//	t.id = id
+//}
+//
 
-func (t *Task) GetId() string {
-	return t.id
+func (t Task) GetOwnerId() string {
+	return t.OwnerId
 }
 
-func (t *Task) SetId(id string) {
-	t.id = id
-}
-
-func (t *Task) GetOwnerId() string {
-	return t.ownerId
-}
-
-func (t *Task) SetOwnerId(newOwnerId string) {
-	t.ownerId = newOwnerId
-}
-
-func (t *Task) GetStatus() Status {
-	return t.status
-}
-
-func (t *Task) SetStatus(newStatus Status) {
-	t.status = newStatus
-}
+//
+//func (t *Task) SetOwnerId(newOwnerId string) {
+//	t.ownerId = newOwnerId
+//}
+//
+//func (t *Task) GetStatus() Status {
+//	return t.status
+//}
+//
+//func (t *Task) SetStatus(newStatus Status) {
+//	t.status = newStatus
+//}
--- main.go
+++ main.go
@@ -3,82 +3,23 @@ package main
 import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"miniProject/EntitiesFolder/TaskFolder"
 )
 
-// Golang program to illustrate the
-// concept of inheritance
-
-// declaring a struct
-type Comic struct {
-
-	// declaring struct variable
-	Universe string
-}
-
-// function to return the
-// universe of the comic
-func (comic Comic) ComicUniverse() string {
-
-	// returns comic universe
-	return comic.Universe
-}
-
-// declaring a struct
-type Marvel struct {
-
-	// anonymous field,
-	// this is composition where
-	// the struct is embedded
-	Comic
-}
-
-// declaring a struct
-type DC struct {
-
-	// anonymous field
-	Comic
-}
-
-// main function
 func main() {
+	//t := TaskFolder.Task{OwnerId: "eden"}
+	h := TaskFolder.HomeWork{TaskFolder.Task{"eden"}}
+	fmt.Println(h.Task.GetOwnerId())
 
-	// creating an instance
-	c1 := Marvel{
+	//h := TaskFolder.NewHomeWork("mivne", time.Parse("2006-01-02", "1996-10-14"), "blah")
+	//dbFolder.CreateDb()
+	//fmt.Println("db was created")
+	//p := EntitiesFolder.NewPerson("peleg", "gmail", "go")
+	//fmt.Println("p is", p)
+	//if dbFolder.InsertPerson(p) {
+	//	fmt.Println("succeed to insert: p is", p)
+	//} else {
+	//	fmt.Println("unable to insert: p is", p)
+	//}
 
-		// child struct can directly
-		// access base struct variables
-		Comic{
-			Universe: "MCU",
-		},
-	}
-
-	// child struct can directly
-	// access base struct methods
-
-	// printing base method using child
-	fmt.Println("Universe is:", c1.ComicUniverse())
-
-	c2 := DC{
-		Comic{
-			Universe: "DC",
-		},
-	}
-
-	// printing base method using child
-	fmt.Println("Universe is:", c2.ComicUniverse())
 }
-
-//func main() {
-//
-//	//h := TaskFolder.NewHomeWork("mivne", time.Parse("2006-01-02", "1996-10-14"), "blah")
-//	//dbFolder.CreateDb()
-//	//fmt.Println("db was created")
-//	//p := EntitiesFolder.NewPerson("peleg", "gmail", "go")
-//	//fmt.Println("p is", p)
-//	//if dbFolder.InsertPerson(p) {
-//	//	fmt.Println("succeed to insert: p is", p)
-//	//} else {
-//	//	fmt.Println("unable to insert: p is", p)
-//	//}
-//
-//}
